b7a3166a450080a75dba0186fbd9109dd066c38c
stupidboy2979 <stupidboy2979@qq.com>
2018-05-10 23:03:41 +0800

    增加数据库

--- indexPage/models.py
+++ indexPage/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Circuit(models.Model):
+    trunk = models.IntegerField()
+    port = models.CharField(max_length=20)
+    sgNum = models.CharField(max_length=20)
+
+
+class User(models.Model):
+    name = models.CharField(max_length=20)
+    number = models.CharField(max_length=20)
+    portal = models.CharField(max_length=20)
+    das = models.IntegerField()
+    dpc = models.CharField(max_length=20)
+    creatDate = models.DateField(auto_now_add=False)
+    circuits = models.ManyToManyField(Circuit)
+
+
